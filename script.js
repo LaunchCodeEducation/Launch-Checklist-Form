db7d713955b58d1a39545e8bd006eaeb9a3da77f
JBuck31 <jrbuck31@gmail.com>
2019-10-12 17:46:01 -0500

    must_input_all_fields

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      if(pilotName.value ==="" || copilotName.value ==="" || fuelLevel.value ==="" || cargoMass.value ===""){
+         alert("All fields are required!");
+         event.preventDefault();
+      }
+      });
+   });
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
