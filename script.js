1ac7a407b7c545a2738c14343b54afd198a00e52
AustinAYates <omicron_austin@ymail.com>
2019-10-14 20:28:00 -0500

    initial changes and verification step done

--- script.js
+++ script.js
@@ -11,3 +11,26 @@
 </ol>
 <img src="${}">
 */
+
+window.addEventListener('load', function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let faultyItems = document.getElementById("faultyItems");
+      let pilotName = document.getElementById("pilotName");
+      let copilotName = document.getElementById("copilotName");
+      let fuelLevel = document.getElementById("fuelLevel");
+      let cargoMass = document.getElementById("cargoMass");
+
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+         alert("All fields are required!");
+      } else if (!isNaN(pilotName.value)) {
+         alert('Please enter a valid name for Pilot.');
+      } else if (!isNaN(copilotName.value)) {
+         alert('Please enter a valid name for Copilot.');
+      }
+
+      faultyItems.style.visibility = "visible";
+
+   })
+})
