d384c5b05b5f9f70cbbc023cb90130eb755da303
Krystal Farwig <youngshaowei@gmail.com>
2019-10-18 12:25:57 -0500

    alerts complete

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
-      form.addEventListener("submit", function(event) {
-      alert("submit clicked");
-      });
+   let reqs = document.getElementById("faultyItems");
+   let pilotReady = document.getElementById("pilotStatus");
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let pilotInput = document.querySelector("input[name=pilotName]");
+      let copilotInput = document.querySelector("input[name=copilotName]");
+      let fuelInput = document.querySelector("input[name=fuelLevel]");
+      let massInput = document.querySelector("input[name=cargoMass]");
+      let pilotValue = pilotInput.value;
+      if (pilotInput.value === "" || copilotInput.value === "" || fuelInput.value === "" || massInput.value === "") {
+         alert("All fields are required!");
+         event.preventDefault();
+      } else if (!isNaN(pilotInput.value) || !isNaN(copilotInput.value)){
+         alert("Make sure to enter valid information for each field!");
+         event.preventDefault();   
+      } else if (isNaN(fuelInput.value) || isNaN(massInput.value)) {
+         alert("Make sure to enter valid information for each field!");
+         event.preventDefault();
+      } else {
+         if (fds ) {
+
+         } else {
+         pilotReady.innerHTML = `${pilotValue} is ready for launch!`;
+         }
+      }
    });
-})
+});
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
