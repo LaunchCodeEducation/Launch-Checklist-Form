484b61485d7c50062f8cfbfbe491929211f6246f
uglynora <mangos8@yahoo.com>
2020-05-18 22:30:51 -0400

    completed bonus mission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Write your JavaScript code here!
 window.onload = () =>{
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event){
@@ -50,17 +49,18 @@ window.onload = () =>{
        fetch(URL).then( function(response) {
          response.json().then( function(json) {
             const div = document.getElementById("missionTarget");
+            const randomPlanet = json[Math.floor(Math.random()*json.length)];
             // Add HTML that includes the JSON data
             div.innerHTML = `
             <h2>Mission Destination</h2>
             <ol>
-               <li>Name: ${json[3].name}</li>
-               <li>Diameter: ${json[3].diameter}</li>
-               <li>Star: ${json[3].star}</li>
-               <li>Distance from Earth: ${json[3].distance}</li>
-               <li>Number of Moons: ${json[3].moons}</li>
+               <li>Name: ${randomPlanet.name}</li>
+               <li>Diameter: ${randomPlanet.diameter}</li>
+               <li>Star: ${randomPlanet.star}</li>
+               <li>Distance from Earth: ${randomPlanet.distance}</li>
+               <li>Number of Moons: ${randomPlanet}.moons}</li>
             </ol>
-            <img src="${json[3].image}">
+            <img src="${randomPlanet.image}">
             `;
          });
       });
@@ -68,15 +68,5 @@ window.onload = () =>{
       }
 
    });
+   form.reset();
 }
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
