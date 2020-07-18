b1ea3b3d1ca0525ecaa8d49b0b9ffb1565177d0e
themitcharoni <64182946+themitcharoni@users.noreply.github.com>
2020-07-17 21:29:24 -0500

    update launch info area partial complete

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
-   let form = document.querySelector("form");
+   const form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
-      let pilotName = document.querySelector("input[name=pilotName]");
-      let copilotName = document.querySelector("input[name=copilotName]");
-      let fuelLevel = document.querySelector("input[name=fuelLevel]");
-      let cargoMass = document.querySelector("input[name=cargoMass]");
+      const pilotName = document.querySelector("input[name=pilotName]");
+      const copilotName = document.querySelector("input[name=copilotName]");
+      const fuelLevel = document.querySelector("input[name=fuelLevel]");
+      const cargoMass = document.querySelector("input[name=cargoMass]");
       if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
          alert("All fields are required!");
          event.preventDefault();
@@ -14,15 +14,38 @@ window.addEventListener("load", function() {
          alert("Fuel Level and Cargo Mass must be numbers")
          event.preventDefault();
       }
+      if (fuelLevel.value < 10000) {
+         document.getElementById("faultyItems").style.visibility = "visible";
+         document.getElementById("fuelStatus").innerHTML = `Not enough Fuel for Journey (must be 10k liters or more)`;
+         document.getElementById("launchStatus").style.color = "red";
+         document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch`
+         event.preventDefault();
+      }else{
+         document.getElementById("fuelStatus").innerHTML = `Fuel level high enough for launch`;
+      }
+      if (cargoMass.value > 10000) {
+         document.getElementById("faultyItems").style.visibility = "visible";
+         document.getElementById("cargoStatus").innerHTML = 'Too much mass for shuttle to take off (must be less than 10k kg)';
+         document.getElementById("launchStatus").style.color = "red";
+         document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch`
+         event.preventDefault();
+      }else{
+         document.getElementById("cargoStatus").innerHTML = 'Cargo mass low enough for launch';
+      }
       if (typeof pilotName.value !== "string" || typeof copilotName.value !== "string" ) {
          alert("Pilot names must be letters only")
          event.preventDefault();
       }
+      // document.getElementById("faultyItems").style.visibility = "visible";
+      document.getElementById("pilotStatus").innerHTML = `${pilotName.value} Ready`;
+      document.getElementById("copilotStatus").innerHTML = `${copilotName.value} Ready`;
+      // document.getElementById("faultyItems").innerHTML =`<li id="pilotStatus"> ${pilotName.value} Ready</li><li id="copilotStatus">${copilotName.value} Ready</li><li id="fuelStatus">Fuel level high enough for launch</li><li id="cargoStatus">Cargo mass low enough for launch</li></ol>`;
    });
+   
 });
 
 
-
+event.preventDefault();
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
