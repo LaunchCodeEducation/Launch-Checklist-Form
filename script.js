1a2571351be418f4cddee437485476bc5dbb7154
Allison <abarnett17@gmail.com>
2020-10-29 10:02:53 -0500

    Let's hope this works

--- script.js
+++ script.js
@@ -1,13 +1,126 @@
-// Write your JavaScript code here!
+window.addEventListener("load", function(){
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event){
+      event.preventDefault();     
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+       let pilotNameInput = document.querySelector("input[name=pilotName]").value;
+       let coPilotNameInput = document.querySelector("input[name=copilotName]").value;
+       let fuelLevelInput = document.querySelector("input[name=fuelLevel]").value;
+       let cargoMassInput = document.querySelector("input[name=cargoMass]").value;
+
+       let faultyItemList = document.getElementById("faultyItems");
+       let fuelStatus = document.getElementById("fuelStatus");
+       let launchStatus = document.getElementById("launchStatus");
+       let cargoStatus = document.getElementById("cargoStatus");
+       let pilotStatus = document.getElementById("pilotStatus");
+       let copilotStatus = document.getElementById("copilotStatus");
+
+
+
+       if (pilotNameInput.value === "" || coPilotNameInput.value === "" 
+       || fuelLevelInput.value === "" || cargoMassInput.value === ""){
+       alert("All fields are required");
+      event.preventDefault(); 
+      }   
+
+      if (!isNaN(pilotNameInput)){
+         window.alert("Enter a Valid Name");
+         event.preventDefault();
+         launchStatus = false;
+      }else{
+          pilotStatus.innerHTML = `Pilot ${pilotNameInput} is ready for launch`;
+      }
+      if (!isNaN(coPilotNameInput)){
+         window.alert("Enter a Valid Name");
+         event.preventDefault();
+         launchStatus = false;
+      }else{
+          copilotStatus.innerHTML = `CoPilot ${coPilotNameInput} is ready for launch`;
+      }
+
+      if(fuelLevelInput < 10000){
+         launchStatus.style.color = "red";
+         launchStatus.innerHTML = "Shuttle not ready for launch";
+         fuelStatus.innerHTML = "Fuel level too low for launch";
+      }
+
+      if(cargoMassInput > 10000){
+         launchStatus.style.color = "red";
+         launchStatus.innerHTML = "Shuttle not ready for launch";
+         cargoStatus.innerHTML = "Cargo mass too high for launch";
+      }
+
+
+
+      // if (fuelLevelInput < 10000){ 
+      //    launchStatus.style.color = "red";
+      //    launchStatus.innerHTML = "Shuttle not ready for launch";
+      // } else if (cargoMassInput > 10000){
+      //    launchStatus.style.color = "red";
+      //    launchStatus.innerHTML = "Shuttle not ready for launch"
+      // } else {
+      //    launchStatus.style.color = "green";
+      //    launchStatus.innerHTML = "Shuttle is ready for launch";
+      // }
+
+      
+      if (Number(fuelLevelInput) === true){
+          fuelLevelInput;
+      }
+      if (Number(cargoMassInput) === true){
+          cargoMassInput;
+   
+       } else {
+         faultyItemList.style.visibility = "visible";
+         pilotStatus.innerHTML = `Pilot ${pilotNameInput} is ready for launch`;
+         copilotStatus.innerHTML = `CoPilot ${coPilotNameInput} is ready for launch`;
+       }
+         //  else if (isNaN(pilotNameInput) || isNaN(coPilotNameInput) || 
+      //  Number(fuelLevelInput) === true || Number(cargoMassInput) === true){
+      //    alert("Enter Valid Information");
+         // if (fuelLevelInput < 10,000){ 
+         //    launchStatus.style.color = "red";
+         //    launchStatus.innerHTML = "Shuttle not ready for launch";
+         // } else if (cargoMassInput > 10,000){
+         //    launchStatus.style.color = "red";
+         //    launchStatus.innerHTML = "Shuttle not ready for launch"
+         // } else {
+         //    launchStatus.style.color = "green";
+         //    launchStatus.innerHTML = "Shuttle is ready for launch";
+         // }
+         // When I uncomment this, the planet info doesnt load at all
+       
+      //  if (fuelLevelInput < 10,000){ 
+      //    launchStatus.style.color = "red";
+      //    launchStatus.innerHTML = "Shuttle not ready for launch";
+      // } else if (cargoMassInput > 10,000){
+      //    launchStatus.style.color = "red";
+      //    launchStatus.innerHTML = "Shuttle not ready for launch"
+      // } else {
+      //    launchStatus.style.color = "green";
+      //    launchStatus.innerHTML = "Shuttle is ready for launch";
+      // }
+       
+   });
+
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+   response.json().then(function(json){
+      let missionTargetInfo = document.getElementById("missionTarget");
+      let jsonIndex = Math.floor(Math.random()*json.length);
+      let planetInfo = json[jsonIndex];
+      missionTargetInfo.innerHTML = `
+      <h2>Mission Destination</h2>
+   <ol>
+      <li>Name: ${planetInfo.name}</li>
+      <li>Diameter: ${planetInfo.diameter}</li>
+      <li>Star: ${planetInfo.star}</li>
+      <li>Distance from Earth: ${planetInfo.distance}</li>
+      <li>Number of Moons: ${planetInfo.moons}</li>
+   </ol>
+   <img src="${planetInfo.image}"></img>`
+   });
+
+});
+});
+      
+
