5435e01f0b642ae764a63d8d18492a1dfe1298d2
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-06-17 15:30:40 -0500

    started assignment, preventDefault

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // Write your JavaScript code here!
 
+window.addEventListener("load", function() {
+   let launchForm = document.getElementById("launchForm");
+   launchForm.addEventListener("submit", function(event) {
+      let pilotName = document.getElementById("pilotName");
+      let copilotName = document.getElementById("copilotName");
+      let fuelLevel = document.getElementById("fuelLevel");
+      let cargoMass = document.getElementById("cargoMass");
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+         alert("All fields required!");
+         event.preventDefault();
+      }   
+   });
+});
+
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
